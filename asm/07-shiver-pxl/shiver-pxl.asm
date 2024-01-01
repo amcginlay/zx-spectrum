b469141bbeca595f8a4b68217491c857c02aeb52
Alan McGinlay <amcginlay@gmail.com>
2024-01-01 18:16:32 +0000

    use lastk to abort on any keypress

--- asm/07-shiver-pxl/shiver-pxl.asm
+++ asm/07-shiver-pxl/shiver-pxl.asm
@@ -1,8 +1,12 @@
-                                ; compile with "pasmo --name shiver-pxl --tap shiver-pxl.asm shiver-pxl.tap"
+                                ; compile with "pasmo --name shiver-pxl --tapbas shiver-pxl.asm shiver-pxl.tap"
                 org  $FF58      ; address where you load the program (65368 as per machine-code-loader)
 
-next_iter:
-                ld   hl, 22527
+zero_out_lastk: ld   hl, 23560  ; lastk                                        
+                ld   a, 255                                           
+                ld   (hl), a          
+
+start:
+left:           ld   hl, 22527
                 ld   c, 192
 next_line_l:    ld   b, 32
                 or   a
@@ -12,7 +16,7 @@ next_byte_l:    rl   (hl)
                 dec  c
                 jr   nz, next_line_l
 
-                ld   hl, 16384
+right:          ld   hl, 16384
                 ld   c, 192
 next_line_r:    ld   b, 32
                 or   h
@@ -20,10 +24,12 @@ next_byte_r:    rr   (hl)
                 inc  hl
                 djnz next_byte_r
                 dec  c
-                jr nz, next_line_r
-
-                jp next_iter
+                jr   nz, next_line_r      
+                                
+check_key:      ld   hl, 23560 ; lastk
+                cp   (hl)                                            
+                jr   z, start  ; 255 means no key pressed -> continue                                   
 
                 ret
 
-;                end  $FF58
+                ;end  $FF58      ; determines if RAND USR added to BASIC stubs
